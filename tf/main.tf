256e94abacec6e2a3cf9ecccb8f26c8011c1d561
Aleksandar Cevap <aleksandarcevap@gmail.com>
2025-07-28 21:44:56 +0200

    Add ssh pub key for connecting to vm into workflow and into tf code

--- tf/main.tf
+++ tf/main.tf
@@ -42,7 +42,7 @@ resource "azurerm_linux_virtual_machine" "hsx-vm" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = var.ssh_public_key
   }
 
   os_disk {
