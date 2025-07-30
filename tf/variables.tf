780ab183b72c52483e0199a3f510eb3600783619
Aleksandar Cevap <aleksandarcevap@gmail.com>
2025-07-30 19:10:50 +0200

    Execute tf fmt command

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,40 +1,40 @@
 variable "ssh_public_key" {
-  type = string
+  type        = string
   description = "The SSH public key to access the VM"
 }
 
 variable "location" {
   description = "Azure Region to deploy resources"
-  type = string
-  default = "West Europe"
+  type        = string
+  default     = "West Europe"
 }
 
 variable "project_name" {
   description = "The name prefix for all resources"
-  type = string
-  default = "ta-hsx"
+  type        = string
+  default     = "ta-hsx"
 }
 
 variable "admin_username" {
   description = "VM Admin username"
-  type = string
-  default = "adminuser"
+  type        = string
+  default     = "adminuser"
 }
 
 variable "vm_size" {
   description = "VM Size"
-  type = string
-  default = "Standard_A2_v2"
+  type        = string
+  default     = "Standard_A2_v2"
 }
 
 variable "vnet_address_space" {
   description = "CIDR block for the VNET"
-  type = list(string)
-  default = ["10.0.0.0/26"]
+  type        = list(string)
+  default     = ["10.0.0.0/26"]
 }
 
 variable "subnet_address_space" {
   description = "CIDR block for the VM subnet"
-  type = list(string)
-  default = ["10.0.0.0/27"]
+  type        = list(string)
+  default     = ["10.0.0.0/27"]
 }
